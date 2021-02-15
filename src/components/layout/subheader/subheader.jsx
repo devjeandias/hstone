b41f0cd9e5fbd543c4fa27ba1e6e36c794507a2e
Jean Dias <devjeandias@gmail.com>
2021-02-15 06:12:47 -0300

    componente html subheader

---
+++ src/components/layout/subheader/subheader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './subheader.css'
+
+export default function() {
+    return(
+        <div className="main--header container">
+            <h1 className="title">Cartas</h1>
+            <span className="subtitle">Total de cartas: 30</span>
+        </div>
+    )
+}
